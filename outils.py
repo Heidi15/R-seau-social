3be453c1a0b5f1b162555c4a93a019f1a04b2210
JinSeeng <sellia.nade@gmail.com>
2025-01-06 15:30:23 +0100

    Implement class File

--- outils.py
+++ outils.py
@@ -17,3 +17,23 @@ class Pile:
         if self.est_vide():
             raise IndexError("La pile est vide")
         return self.elements[-1]
+
+class File:
+    def __init__(self):
+        self.elements = []
+    
+    def est_vide(self):
+        return len(self.elements) == 0
+    
+    def enfiler(self, element):
+        self.elements.append(element)
+    
+    def defiler(self):
+        if self.est_vide():
+            raise IndexError("Impossible de défiler une file vide")
+        return self.elements.pop(0)
+    
+    def premier(self):
+        if self.est_vide():
+            raise IndexError("La file est vide")
+        return self.elements[0]

26c22c9d92217cded554a4be3efd718f94e5d832
JinSeeng <sellia.nade@gmail.com>
2025-01-07 20:22:44 +0100

    Update file "test_aretes"

--- test_aretes.py
+++ test_aretes.py
@@ -1,1 +1,18 @@
 from outils import Aretes
+
+def test_aretes():
+    matrice = [
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
+    ]
+    a_matrice = Aretes(matrice)
+    assert a_matrice.aretesmatrice() == 2
+
+    liste = [[0, 1], [1, 2], [2, 0]]
+    a_liste = Aretes(liste)
+    assert a_liste.aretesliste() == 3
+
+if __name__ == '__main__':
+    test_aretes()
+    print("Tous les tests ont réussi.")

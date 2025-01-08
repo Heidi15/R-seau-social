9d9f95d76e4481fda97f2989d9623927e5e7a558
JinSeeng <sellia.nade@gmail.com>
2025-01-08 11:23:30 +0100

    Update test_influenceur.py
    Tests to check that the function is working :
    - in a simple case
    - if the network is empty
    - with only one influencer
    - with only one person

---
+++ test_influenceur.py
@@ -0,0 +1,34 @@
+import unittest
+from outils import plus_grands_influenceurs
+
+class TestPlusGrandsInfluenceurs(unittest.TestCase):
+    def test_cas_simple(self):
+        reseau = {
+            'Sellia': ['Léa', 'Heidi'],
+            'Léa': ['Heidi'],
+            'Heidi': ['Léa']
+        }
+        self.assertEqual(plus_grands_influenceurs(reseau), ['Léa', 'Heidi'])
+    
+    def test_reseau_vide(self):
+        reseau = {}
+        self.assertEqual(plus_grands_influenceurs(reseau), [])
+    
+    def test_unique_influenceur(self):
+        reseau = {
+            'Sellia': ['Heidi'],
+            'Léa': ['Heidi'],
+            'Heidi': []
+        }
+        self.assertEqual(plus_grands_influenceurs(reseau), ['Heidi'])
+    
+    def test_personne_isolee(self):
+        reseau = {
+            'Sellia': [],
+            'Léa': [],
+            'Heidi': []
+        }
+        self.assertEqual(set(plus_grands_influenceurs(reseau)), {'Sellia', 'Léa', 'Heidi'})
+
+if __name__ == '__main__':
+    unittest.main()

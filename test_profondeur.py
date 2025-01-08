e9a994212fcdbf484c5d1c6cf7b86a123ca2cc48
JinSeeng <sellia.nade@gmail.com>
2025-01-08 14:10:45 +0100

    Create function "lire_graphe"
    - Add new function "lire_graphe" that reads the file go-9-01.txt and creates the corresponding adjacency matrix

--- test_profondeur.py
+++ test_profondeur.py
@@ -1,1 +1,19 @@
 from outils import parcours_profondeur
+
+def lire_graphe(nom_fichier):
+    with open(nom_fichier, 'r') as f:
+        lignes = f.readlines()
+        
+    # Lecture de l'en-tête
+    nb_sommets = int(lignes[1])
+    
+    # Création de la matrice d'adjacence initialisée à 0
+    matrice = [[0 for _ in range(nb_sommets)] for _ in range(nb_sommets)]
+    
+    # Lecture des arcs et remplissage de la matrice
+    for ligne in lignes[nb_sommets+2:]:  # 
+        if ligne.strip():  # Ignore les lignes vides
+            source, destination = map(int, ligne.strip().split())
+            matrice[source][destination] = 1
+            
+    return matrice

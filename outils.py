8258cd328588880e6e0697748e630e72c928b065
JinSeeng <sellia.nade@gmail.com>
2025-01-08 11:18:04 +0100

    Add comments and docstring to class "Influenceur"

--- outils.py
+++ outils.py
@@ -40,20 +40,35 @@ class File:
     
 class Influenceur:
     def plus_grands_influenceurs(reseau):
+        """
+        Identifie la/les personne(s) la/les plus suivie(s) dans le réseau.
+        
+        Args:
+            reseau (dict): Dictionnaire avec comme clés les personnes et comme valeurs 
+                        les listes des personnes qu'elles suivent
+        
+        Returns:
+            list: Liste des personnes ayant le plus grand nombre de followers
+        """
+        # Compter les followers pour chaque personne
         nb_followers = {}
-
+        
+        # Initialiser le compteur pour chaque personne du réseau
         for personne in reseau:
             nb_followers[personne] = 0
         
+        # Compter les followers
         for personne, suivis in reseau.items():
             for suivi in suivis:
                 if suivi in nb_followers:
                     nb_followers[suivi] += 1
         
+        # Trouver le nombre maximum de followers
         max_followers = 0
         if nb_followers:
             max_followers = max(nb_followers.values())
         
+        # Identifier toutes les personnes ayant ce nombre maximum de followers
         influenceurs = [
             personne for personne, followers in nb_followers.items()
             if followers == max_followers

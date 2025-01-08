403a99b17bfba4dc805be49cf3af12130ecd7c2e
JinSeeng <sellia.nade@gmail.com>
2025-01-08 14:59:49 +0100

    Move class Temps_propa to module "outils"
    - Change location of class "Temps_propa" from deleted file to module outils

--- outils.py
+++ outils.py
@@ -37,3 +37,28 @@ class File:
         if self.est_vide():
             raise IndexError("La file est vide")
         return self.elements[0]
+    
+class Temps_propa:
+    def __init__(self):
+        self.res= {}
+
+    def relation(self, utilisateur, suivi):
+        if utilisateur not in self.res:
+            self.res[utilisateur]= []
+        self.res[utilisateur].append(suivi)
+    
+    def temps(self, source, cible):
+        file=[(source, 0)]
+        visite= set([source])
+
+        while file:
+            explorer, profondeur= file.pop(0)
+            if explorer== cible:
+                return profondeur*5
+            
+            for voisin in self.res.get(explorer,[]):
+                if voisin not in visite:
+                    visite.add(voisin)
+                    file.append((voisin, profondeur+1))
+
+        return -1    

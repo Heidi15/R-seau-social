f39fdeb8b5a14f4f4b601d267d449cff9d3a4bf4
JinSeeng <sellia.nade@gmail.com>
2025-01-10 09:49:11 +0100

    Move function from deleted file to module outils

--- outils.py
+++ outils.py
@@ -37,3 +37,26 @@ class File:
         if self.est_vide():
             raise IndexError("La file est vide")
         return self.elements[0]
+    
+class Chemin_Propagation:
+    def chemin_minimum(graphe, source, destination):
+        file = [source]  
+        predecesseur = {source: None}  
+        
+        while file:
+            personne_actuelle = file.pop(0)
+
+            if personne_actuelle == destination:
+                chemin = []
+                while personne_actuelle is not None:
+                    chemin.append(personne_actuelle)
+                    personne_actuelle = predecesseur[personne_actuelle]
+                return chemin[::-1]  
+
+            for voisin in graphe.get(personne_actuelle, []):
+                if voisin not in predecesseur:  
+                    file.append(voisin)
+                    predecesseur[voisin] = personne_actuelle
+
+
+        return None
